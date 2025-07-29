98e46d281029847385831ccf3f1a03273fa2232b
Filipe Camargo Marques <filipe.marques@compasso.com.br>
2025-07-29 15:05:10 -0300

    My first commit

---
+++ ChainFlow/ChainFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChainFlowApp.swift
+//  ChainFlow
+//
+//  Created by Filipe Camargo Marques on 29/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ChainFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
